337b846ecdf7d07ea9e35c874b70025109dd3ff5
jaz2000 <jazeelanwar@gmail.com>
2023-07-06 13:01:48 +0530

    studied about class and constructor

--- person.js
+++ person.js
@@ -1,6 +1,18 @@
-const person={
-    name : 'John Doe',
-    age : 30
+// const person={
+//     name : 'John Doe',
+//     age : 30
+// }
+
+// module.exports=person;
+
+class Person{
+    constructor(name,age){
+        this.name=name;
+        this.age=age;
+    }
+    greeting(){
+        'My name is ${this.name} and I am ${this.age} years old';
+    }
 }
 
-module.exports=person;
+module.exports=Person;
