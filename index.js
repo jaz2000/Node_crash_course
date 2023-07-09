fe5e3733697a8152a2ecc1045d33e271f860c3fd
jaz2000 <jazeelanwar@gmail.com>
2023-07-09 15:15:29 +0530

    created server

--- index.js
+++ index.js
@@ -15,3 +15,19 @@
 // logger.log('Hi');
 // logger.log('Hello');
 
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer(
+    (req,res)=>{
+        // console.log(req.url);
+        if(req.url=='/'){
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.end('<h1>Home</h1>');
+        }
+    }
+);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT,()=>console.log(`server running on port ${PORT}`));
